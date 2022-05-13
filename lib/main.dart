9d6c96877b6b39ab4d508416297b60ac8c57c50d
Dan-Sutton <dewasutton@gmail.com>
2022-05-13 09:30:36 +0100

    chnaged color of background

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GoMoon',
+      theme:
+          ThemeData(scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 1.0)),
       home: Scaffold(),
     );
   }
