52fd325ad1d7f4db65f81e1078a02324d6f4208c
Dan-Sutton <dewasutton@gmail.com>
2022-05-13 10:20:33 +0100

    Added Page Text

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -6,11 +6,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/astro_moon.png"))),
-      ),
+      body: _pageTitle(),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GOMOON",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage("assets/images/astro_moon.png"))),
     );
   }
 }
